8a0b3539b2bce2d4d8c962a4c2dd2a80445a1354
Brian Morillo <brianmorillog@gmail.com>
2024-03-22 20:02:58 -0400

    Spring Boot Backend directory

---
+++ backend/src/main/java/com/g5/brainflash/BrainflashApplication.java
@@ -0,0 +1,13 @@
+package com.g5.brainflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainflashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainflashApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/g5/brainflash/BrainflashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g5.brainflash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainflashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
