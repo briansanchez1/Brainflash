be6be8bf34f76b19658ad6c243941c23f8411e56
Brian Morillo <brianmorillog@gmail.com>
2024-03-24 08:43:29 -0400

    Uncomments authenticate method in the AuthenticationController(Previously commented for testing purposes)

--- backend/src/main/java/com/g5/brainflash/auth/AuthenticationController.java
+++ backend/src/main/java/com/g5/brainflash/auth/AuthenticationController.java
@@ -49,12 +49,12 @@ public class AuthenticationController {
         }
         
     }
-/*
+
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
         @RequestBody AuthenticationRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
-    }    */
+    }    
 
 }
