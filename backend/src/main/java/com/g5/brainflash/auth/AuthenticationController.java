b618ade214a3db716af7ef3be678794fe183839b
Brian Morillo <brianmorillog@gmail.com>
2024-03-25 19:10:01 -0400

    Removed unnecessary CrossOrigin import from AuthenticationController.java

--- backend/src/main/java/com/g5/brainflash/auth/AuthenticationController.java
+++ backend/src/main/java/com/g5/brainflash/auth/AuthenticationController.java
@@ -3,7 +3,6 @@ package com.g5.brainflash.auth;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin ;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
