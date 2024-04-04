95c0c8c3c93da2ce09e05219429456fe2efff244
briansanchez1 <sanchezmelob@wit.edu>
2024-04-04 19:01:33 -0400

    Update pfe_card.jsx

--- frontend/src/components/pfe_card.jsx
+++ frontend/src/components/pfe_card.jsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import { CardContent, Card, CardActionArea } from "@mui/material";
 
-const handleClick = (pfe) => {
+const handleClick = (pfe ) => {
   console.log("Clicked category:", pfe);
 };
 
 export default function pfe_card({ pfe }) {
+
+
   return (
     <Card>
       <CardActionArea onClick={() => handleClick(pfe)}>
