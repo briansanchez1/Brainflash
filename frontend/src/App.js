54ea55606de37aa5bc680056429826c3bad86680
Brian Morillo <brianmorillog@gmail.com>
2024-04-07 02:55:39 -0400

    Added a Brainflash context that provides flashcard, deck, category and sessions states to be used  throughout the application in CRUD form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, useParams } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import ForgotPass from "./pages/forgot_password";
@@ -8,12 +13,12 @@ import Categories from "./pages/categories";
 import Decks from "./pages/decks";
 import PFE from "./pages/pfe";
 import Flashcards from "./pages/flashcards";
-import Flashcard from "./pages/flashcard";
 import FlashcardsByCategory from "./pages/flashcards_by_category";
 import FlashcardsByDeck from "./pages/flashcards_by_deck";
 import PageNotFound from "./pages/page_not_found";
 import MainLayout from "./layouts/main_layout";
 import PrivateRoutes from "./components/private_routes";
+import { BrainflashProvider } from "./components/context/brainflash_context";
 
 function FlashcardsInCategoryPage() {
   let { categoryId } = useParams();
@@ -33,81 +38,83 @@ function FlashcardsInDeckPage() {
       <FlashcardsByDeck deckId={deckId} />
     </MainLayout>
   );
-
 }
 
 function App() {
-
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route element={<PrivateRoutes />}>
-            <Route
-              path="/"
-              element={
-                <MainLayout>
-                  <Dashboard />
-                </MainLayout>
-              }
-            />
-            <Route
-              path="/categories"
-              element={
-                <MainLayout>
-                  <Categories />
-                </MainLayout>
-              }
-            />
-            <Route
-              path="/pfe"
-              element={
-                <MainLayout>
-                  <PFE />
-                </MainLayout>
-              }
-            />
+    <BrainflashProvider>
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+            <Route element={<PrivateRoutes />}>
+              <Route
+                path="/"
+                element={
+                  <MainLayout>
+                    <Dashboard />
+                  </MainLayout>
+                }
+              />
+              <Route
+                path="/categories"
+                element={
+                  <MainLayout>
+                    <Categories />
+                  </MainLayout>
+                }
+              />
+              <Route
+                path="/pfe"
+                element={
+                  <MainLayout>
+                    <PFE />
+                  </MainLayout>
+                }
+              />
 
-            <Route
-              path="/decks"
-              element={
-                <MainLayout>
-                  <Decks />
-                </MainLayout>
-              }
-            />
-            <Route
-              path="/flashcards"
-              element={
-                <MainLayout>
-                  <Flashcards />
-                </MainLayout>
-              }
-            />
-            <Route path="/flashcards/category">
-              <Route path=":categoryId" element={<FlashcardsInCategoryPage />} />
+              <Route
+                path="/decks"
+                element={
+                  <MainLayout>
+                    <Decks />
+                  </MainLayout>
+                }
+              />
+              <Route
+                path="/flashcards"
+                element={
+                  <MainLayout>
+                    <Flashcards />
+                  </MainLayout>
+                }
+              />
+              <Route path="/flashcards/category">
+                <Route
+                  path=":categoryId"
+                  element={<FlashcardsInCategoryPage />}
+                />
+              </Route>
+
+              <Route path="/flashcards/deck">
+                <Route path=":deckId" element={<FlashcardsInDeckPage />} />
+              </Route>
             </Route>
 
-            <Route path="/flashcards/deck">
-              <Route path=":deckId" element={<FlashcardsInDeckPage />} />
-            </Route>
-            
-          </Route>
-
-          <Route path="/forgot-password" element={<ForgotPass />}></Route>
-          <Route
-            path="*"
-            element={
-              <MainLayout>
-                <PageNotFound />
-              </MainLayout>
-            }
-          />
-        </Routes>
-      </Router>
-    </div>
+            <Route path="/forgot-password" element={<ForgotPass />}></Route>
+            <Route
+              path="*"
+              element={
+                <MainLayout>
+                  <PageNotFound />
+                </MainLayout>
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
+    </BrainflashProvider>
   );
 }
 
---
+++ frontend/src/components/context/brainflash_context.jsx
@@ -0,0 +1,128 @@
+import React, { createContext, useState } from "react";
+
+// Create the context
+export const BrainflashContext = createContext();
+
+// Create a provider component
+export const BrainflashProvider = ({ children }) => {
+  const [flashcards, setFlashcards] = useState([]);
+  const [sessions, setSessions] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [decks, setDecks] = useState([]);
+
+  // Add a flashcard
+  const addFlashcard = (flashcard) => {
+    setFlashcards([...flashcards, flashcard]);
+  };
+
+  // Remove a flashcard
+  const removeFlashcard = (id) => {
+    setFlashcards(flashcards.filter((card) => card.id !== id));
+  };
+
+  const updateFlashcard = (flashcard) => {
+    setFlashcards(
+      flashcards.map((f) => {
+        if (f.id === flashcard.id) {
+          f = flashcard;
+        }
+        return f;
+      })
+    );
+  };
+
+  // Add a session
+  const addSession = (session) => {
+    setSessions([...sessions, session]);
+  };
+
+  // Remove a session
+  const removeSession = (id) => {
+    setSessions(sessions.filter((session) => session.id !== id));
+  };
+
+  // Update a session
+  const updateSession = (session) => {
+    setSessions(
+      sessions.map((s) => {
+        if (s.id === session.id) {
+          s = session;
+        }
+        return s;
+      })
+    );
+  };
+
+  // Add a category
+  const addCategory = (category) => {
+    setCategories([...categories, category]);
+  };
+
+  // Remove a category
+  const removeCategory = (id) => {
+    setCategories(categories.filter((category) => category.id !== id));
+  };
+
+  // Update a category
+  const updateCategory = (category) => {
+    setCategories(
+      categories.map((c) => {
+        if (c.id === category.id) {
+          c = category;
+        }
+        return c;
+      })
+    );
+  };
+
+  // Add a deck
+  const addDeck = (deck) => {
+    setDecks([...decks, deck]);
+  };
+
+  // Remove a deck
+  const removeDeck = (id) => {
+    setDecks(decks.filter((deck) => deck.id !== id));
+  };
+
+  // Update a deck
+  const updateDeck = (deck) => {
+    setDecks(
+      decks.map((d) => {
+        if (d.id === deck.id) {
+          d = deck;
+        }
+        return d;
+      })
+    );
+  };
+
+  return (
+    <BrainflashContext.Provider
+      value={{
+        flashcards,
+        setFlashcards,
+        addFlashcard,
+        removeFlashcard,
+        updateFlashcard,
+        sessions,
+        setSessions,
+        addSession,
+        removeSession,
+        updateSession,
+        categories,
+        setCategories,
+        addCategory,
+        removeCategory,
+        updateCategory,
+        decks,
+        setDecks,
+        addDeck,
+        removeDeck,
+        updateDeck,
+      }}
+    >
+      {children}
+    </BrainflashContext.Provider>
+  );
+};
