5292fe6ec26e75ea8abe0b1cecb4b44776086af1
briansanchez1 <sanchezmelob@wit.edu>
2024-04-09 00:14:31 -0400

    fixed prop

--- frontend/src/components/text_fields.jsx
+++ frontend/src/components/text_fields.jsx
@@ -4,9 +4,16 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#E6A4B4",
+    },
+  },
+});
 export function SearchField({ onSearch, fullWidth }) {
   return (
-    <ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
     <Box
       sx={{
         display: "flex",
