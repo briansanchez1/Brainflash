6713835281b4682cd6cb3e4d2d1a5fff6eb72e83
briansanchez1 <sanchezmelob@wit.edu>
2024-04-09 11:16:44 -0400

    removed  unneeded imports in the backend

--- backend/src/main/java/com/g5/brainflash/user/UserController.java
+++ backend/src/main/java/com/g5/brainflash/user/UserController.java
@@ -2,9 +2,7 @@ package com.g5.brainflash.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
